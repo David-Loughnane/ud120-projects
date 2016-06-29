784753e2f255d2e869af13d46441ec46d93b5fdb
David Loughnane <david.loughnane1@gmail.com>
2016-06-29 17:24:48 +0100

    completed multivariate regression and decision trees

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 2 (SVM) mini-project.
-
-    Use a SVM to identify emails from the Enron corpus by their authors:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
 import sys
 from time import time
 sys.path.append("../tools/")
+#from pympler import tracker
+from memory_profiler import memory_usage
+
+import numpy as np
+from sklearn.svm import GaussianNB
+from sklearn import metrics
+
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+#memory_tracker = tracker.SummaryTracker()
+print("Memory usage before: {}MB".format(memory_usage()))
+
+### FEATURE EXTRACTION ####
 features_train, features_test, labels_train, labels_test = preprocess()
+print("features: ", features_train.shape)
 
+#### INSTATNTIATE CLASSIFIER ####
+SVMclassifier = GaussianNB()
 
+#### TRAIN #####
+t0 = time()
+SVMclassifier.fit(features_train, labels_train)
+t1 = time()
+print("Training time: {} seconds".format(round(t1-t0,3)))
 
+#### PREDICT ####
+t0 = time()
+SVMpreditiction = SVMclassifier.predict(features_test)
+t1 = time()
+print("Prediction time: {} seconds".format(round(t1-t0,3)))
+print("Prediciton accuracy: {:.2%}".format(metrics.accuracy_score(labels_test, SVMpreditiction)))
 
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+#memory_tracker.print_diff()
+print("Memory usage after: {}MB".format(memory_usage()))
