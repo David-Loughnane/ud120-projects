0663df0c6adeedad73605d087a22d6fcaa365ca3
David Loughnane <david.loughnane1@gmail.com>
2016-07-02 21:21:44 +0100

    evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,31 +1,39 @@
-#!/usr/bin/python
-
-
-"""
-    Starter code for the evaluation mini-project.
-    Start by copying your trained/tested POI identifier from
-    that which you built in the validation mini-project.
-
-    This is the second step toward building your POI identifier!
-
-    Start by loading/formatting the data...
-"""
-
 import pickle
 import sys
 sys.path.append("../tools/")
+
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+predicitions = clf.predict(features_test)
+accuracy = metrics.accuracy_score(labels_test, predicitions)
+precision = metrics.precision_score(labels_test, predicitions) 
+recall = metrics.recall_score(labels_test, predicitions)
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
+
+conf_matrix = metrics.confusion_matrix(labels_test, predicitions)
+print("Confusion matrix: ") 
+print(conf_matrix)
 
 
-### your code goes here 
-
-
+target_names = ['blue collar', 'POI']
+print(metrics.classification_report(labels_test, predicitions, target_names = target_names))
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -1,32 +1,32 @@
-#!/usr/bin/python
-
-
-"""
-    Starter code for the validation mini-project.
-    The first step toward building your POI identifier!
-
-    Start by loading/formatting the data
-
-    After that, it's not our code anymore--it's yours!
-"""
-
 import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys='../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
 
-### it's all yours from here forward!  
-
-
+predicitions = clf.predict(features_test)
+accuracy = metrics.accuracy_score(labels_test, predicitions)
+precision = metrics.precision_score(labels_test, predicitions) 
+recall = metrics.recall_score(labels_test, predicitons)
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
