0365098a276018934639a71bb77de3b5e0e9c365
David Loughnane <david.loughnane1@gmail.com>
2016-07-02 12:15:00 +0100

    text learning mini project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -7,21 +7,8 @@ import sys
 
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
-
-"""
-    Starter code to process the emails from Sara and Chris to extract
-    the features and get the documents ready for classification.
-
-    The list of all the emails from Sara are in the from_sara list
-    likewise for emails from Chris (from_chris)
-
-    The actual documents are in the Enron email dataset, which
-    you downloaded/unpacked in Part 0 of the first mini-project. If you have
-    not obtained the Enron email corpus, run startup.py in the tools folder.
-
-    The data is stored in lists and packed away in pickle files at the end.
-"""
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
 
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
@@ -29,47 +16,53 @@ from_chris = open("from_chris.txt", "r")
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
-temp_counter = 0
-
-
+### temp_counter - limit no. emails processed
+#temp_counter = 0
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        #temp_counter += 1
+        #if temp_counter < 100:
+        path = os.path.join('..', path[:-1])
+        print(path)
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        stemmed_email = parseOutText(email)
+        ### use str.replace() to remove any instances of the words
+        words_to_replace = ["sara", "shackleton", "chris", "germani"]
+        for word in words_to_replace:
+            if (word in stemmed_email):
+                stemmed_email = stemmed_email.replace(word, "")
+        #stemmed_removed_email = " ".join([word for word in stemmed_email.split() if word not in words_to_replace])
+        ### append the text to word_data
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        word_data.append(stemmed_email)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == "sara":
+            from_data.append(0)
+        else:
+            from_data.append(1)
 
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        email.close()
 
 
-            email.close()
-
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-
-
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 ### in Part 4, do TfIdf vectorization here
 
+#import list of stopwords from nltk corpus
+#sw = stopwords.words("english")
+#creates a vector of tfidf values with stopwords removed
+tfidfVec = TfidfVectorizer(stop_words='english')
 
+wordVec = tfidfVec.fit_transform(word_data)
+print("Number of words: ", len(tfidfVec.get_feature_names()))
+print("Word 34597: ", tfidfVec.get_feature_names()[34597])
